e0a1320468bbc638f9db0fb3547c9db38914fcf2
jiapeng tong <jiapengt@student.unimelb.edu.au>
2019-09-30 16:05:05 +1000

    update the logic to support real time drawing
    update the logic in drawing area, now we can see the shapes when we are dragging the mouse

--- src/DrawingArea.java
+++ src/DrawingArea.java
@@ -25,6 +25,11 @@ public class DrawingArea extends JPanel {
     // color and type
     private Color color = Color.BLACK;
     private String type = "Free Draw";
+    // different shapes instances, used for real time drawing
+    private Shape shapeLine = null;
+    private Shape shapeCircle = null;
+    private Shape shapeRect = null;
+    private Shape shapeOval = null;
 
     public DrawingArea() {
         setBorder(BorderFactory.createLineBorder(Color.black));
@@ -36,6 +41,10 @@ public class DrawingArea extends JPanel {
         });
 
         addMouseMotionListener(new MouseAdapter() {
+            public void mouseMoved(MouseEvent e) {
+                mouseMovedHandler(e);
+            }
+
             public void mouseDragged(MouseEvent e) {
                 mouseDraggedHandler(e);
             }
@@ -46,6 +55,7 @@ public class DrawingArea extends JPanel {
                 mouseReleasedHandler(e);
             }
         });
+
     }
 
     public Dimension getPreferredSize() {
@@ -77,22 +87,42 @@ public class DrawingArea extends JPanel {
             case "Free Draw":
                 startPointX = e.getX();
                 startPointY = e.getY();
+                shapeLine = new Shape((int) startPointX, (int) startPointY,
+                        (int) startPointX, (int) startPointY, color);
+                shapeLine.setType(type);
+                shapes.add(shapeLine);
                 break;
             case "Line":
                 startPointX = e.getX();
                 startPointY = e.getY();
+                shapeLine = new Shape((int) startPointX, (int) startPointY,
+                        (int) startPointX, (int) startPointY, color);
+                shapeLine.setType(type);
+                shapes.add(shapeLine);
                 break;
             case "Rectangle":
                 startPointX = e.getX();
                 startPointY = e.getY();
+                shapeRect = new Shape((int) startPointX, (int) startPointY,
+                        (int) endPointX, (int) endPointY, color);
+                shapeRect.setType(type);
+                shapes.add(shapeRect);
                 break;
             case "Circle":
                 startPointX = e.getX();
                 startPointY = e.getY();
+                shapeCircle = new Shape((int) startPointX, (int) startPointY,
+                        (int) endPointX, (int) endPointY, color);
+                shapeCircle.setType(type);
+                shapes.add(shapeCircle);
                 break;
             case "Oval":
                 startPointX = e.getX();
                 startPointY = e.getY();
+                shapeOval = new Shape((int) startPointX, (int) startPointY,
+                        (int) endPointX, (int) endPointY, color);
+                shapeOval.setType(type);
+                shapes.add(shapeOval);
                 break;
         }
     }
@@ -105,12 +135,38 @@ public class DrawingArea extends JPanel {
                 Shape shape = new Shape((int) startPointX, (int) startPointY,
                         (int) endPointX, (int) endPointY, color);
                 shape.setType(type);
-                    shapes.add(shape);
+                shapes.add(shape);
                 startPointX = endPointX;
                 startPointY = endPointY;
                 repaint();
                 break;
             case "Line":
+                endPointX = e.getX();
+                endPointY = e.getY();
+                shapeLine.setX2((int) endPointX);
+                shapeLine.setY2((int) endPointY);
+                repaint();
+                break;
+            case "Rectangle":
+                endPointX = e.getX();
+                endPointY = e.getY();
+                shapeRect.setX2((int) endPointX);
+                shapeRect.setY2((int) endPointY);
+                repaint();
+                break;
+            case "Circle":
+                endPointX = e.getX();
+                endPointY = e.getY();
+                shapeCircle.setX2((int) endPointX);
+                shapeCircle.setY2((int) endPointY);
+                repaint();
+                break;
+            case "Oval":
+                endPointX = e.getX();
+                endPointY = e.getY();
+                shapeOval.setX2((int) endPointX);
+                shapeOval.setY2((int) endPointY);
+                repaint();
                 break;
         }
     }
@@ -120,40 +176,21 @@ public class DrawingArea extends JPanel {
             case "Free Draw":
                 break;
             case "Line":
-                endPointX = e.getX();
-                endPointY = e.getY();
-                Shape shapeLine = new Shape((int) startPointX, (int) startPointY,
-                        (int) endPointX, (int) endPointY, color);
-                shapeLine.setType(type);
-                shapes.add(shapeLine);
-                repaint();
                 break;
             case "Rectangle":
-                endPointX = e.getX();
-                endPointY = e.getY();
-                Shape shapeRect = new Shape((int) startPointX, (int) startPointY,
-                        (int) endPointX, (int) endPointY, color);
-                shapeRect.setType(type);
-                shapes.add(shapeRect);
-                repaint();
                 break;
             case "Circle":
-                endPointX = e.getX();
-                endPointY = e.getY();
-                Shape shapeCircle = new Shape((int) startPointX, (int) startPointY,
-                        (int) endPointX, (int) endPointY, color);
-                shapeCircle.setType(type);
-                shapes.add(shapeCircle);
-                repaint();
                 break;
             case "Oval":
+                break;
+        }
+    }
+
+    private void mouseMovedHandler(MouseEvent e) {
+        switch (type) {
+            case "Line":
                 endPointX = e.getX();
                 endPointY = e.getY();
-                Shape shapeOval = new Shape((int) startPointX, (int) startPointY,
-                        (int) endPointX, (int) endPointY, color);
-                shapeOval.setType(type);
-                shapes.add(shapeOval);
-                repaint();
                 break;
         }
     }
